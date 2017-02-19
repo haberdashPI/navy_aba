ee16a33fc86dfec53f76cda8bd6c4f61eda5160b
David Little <david.frank.little@gmail.com>
2017-02-19 07:40:46 -0800

    Use fixed codes for important events. Protocol adjustments
    - shortened practice
    - added one more switch validation block

--- run_aba.jl
+++ run_aba.jl
@@ -1,5 +1,8 @@
 #!/usr/bin/env julia
 
+# TODO: make sure this shows the right number of breaks.
+# TODO: one longer break in the middle
+
 # different code for each of the three repetiats
 # in the trial
 # code for the very first
@@ -26,8 +29,9 @@ stimuli_per_response = 3
 trial_spacing = aba_SOA
 
 n_trials = 600
-num_practice_trials = 20
-n_validate_trials = n_break_after = 75
+num_practice_trials = 12
+n_break_after = 75
+n_validate_trials = 2n_break_after
 
 n_repeat_example = 30
 
@@ -42,8 +46,8 @@ medium = 6st
 medium_str = "6st"
 stimuli = Dict(:low => aba(3st),:medium => aba(medium),:high => aba(18st))
 
-stream_1 = key"p" #key":cedrus2:"
-stream_2 = key"q" #key":cedrus5:"
+stream_1 = key":cedrus2:"
+stream_2 = key":cedrus5:"
 
 isresponse(e) = iskeydown(e,stream_1) || iskeydown(e,stream_2)
 
@@ -84,13 +88,15 @@ function real_trial(stimulus,isfirst;limit=trial_spacing,info...)
   [resp,show_cross(),stimuli,moment(limit)]
 end
 
-function validate_trial(stimulus;limit=trial_spacing,info...)
-   resp = response(stream_1 => "switches_1_or_0",
-                   stream_2 => "switches_2_or_more";info...)
-  stim = [create_aba(stimulus;info...),moment(aba_SOA)]
+function validate_trial(stimulus,isfirst;limit=trial_spacing,info...)
+  resp = response(stream_1 => "switches_1_or_0",
+                  stream_2 => "switches_2_or_more";info...)
 
-  [resp,show_cross(),moment(repeated(stim,stimuli_per_response)),
-   moment(aba_SOA*stimuli_per_response+limit)]
+  stimuli = map(1:stimuli_per_response) do index
+    [create_aba(stimulus,index,isfirst;info...),moment(aba_SOA)]
+  end
+
+  [resp,show_cross(),stimuli,moment(limit)]
 end
 
 
@@ -107,51 +113,63 @@ exp = Experiment(
   moment_resolution=moment_resolution,
 )
 
+function cedrus_instruct(str)
+  text = visual(str*" (Hit \"M\" key to continue...)")
+  m = moment() do
+    record("instructions")
+    display(text)
+  end
+  [m,await_response(iskeydown(key":cedrus3:"))]
+end
+
 setup(exp) do
   addbreak(moment(record,"start"))
 
   addbreak(
-    instruct("""
+    cedrus_instruct("""
 
       In each trial of the present experiment you will hear a series of beeps.
       This may appear to proceeded in a galloping rhythm or it may sound like
       two distinct series of tones."""),
 
-    instruct("""
+    cedrus_instruct("""
 
-      For instance, the following example will normally seem to be
-      galloping."""))
+      For instance, the following example will normally seem to have
+      a galloping-like rhythm."""))
 
   addpractice(show_cross(),
               repeated([create_aba(:low,phase="practice"),moment(aba_SOA)],
                        n_repeat_example))
 
-  addbreak(instruct("""
+  addbreak(cedrus_instruct("""
 
-      On the other hand, normally the following example will eventually seem to
-      be two separate series of tones."""))
+      On the other hand, normally the following example will not appear 
+      to gallop."""))
 
   addpractice(show_cross(),
               repeated([create_aba(:high,phase="practice"),moment(aba_SOA)],
                        n_repeat_example))
 
   addbreak(
-    instruct("""
+    cedrus_instruct("""
 
       In this experiment we'll be asking you to listen for whether it appears
-      that the tones "gallop", or are separate from one antoher."""),
+      that the tones "gallop", or do not."""),
 
-    instruct("""
+    cedrus_instruct("""
 
       Every once in a while, we want you to indicate what you heard most often,
-      a gallop or separate tones. Let's practice a bit.  Use "Q" to indicate
-      that you heard a "gallop" most of the time, and "P" otherwise."""))
+      a gallop or something else. Let's practice a bit.  Use the orange button
+      to indicate that you heard a "gallop" most of the time, and otherwise use
+      the yellow button.
+      
+      """))
 
   addpractice(
     repeated(practice_trial(:medium,phase="practice",limit=10trial_spacing),
              num_practice_trials))
 
-  addbreak(instruct("""
+  addbreak(cedrus_instruct("""
 
     In the real experiment, your time to respond will be limited. Let's
     try another practice round, this time a little bit faster.
@@ -161,16 +179,16 @@ setup(exp) do
     repeated(practice_trial(:medium,phase="practice",limit=2trial_spacing),
              num_practice_trials))
 
-  addbreak(instruct("""
-    In the real experiment, your time to respond will be even more limited.
-    Try to respond before the next trial begins, but even if you don't
-    please still respond."""))
+  addbreak(cedrus_instruct("""
+
+    In the real expeirment, try to respond before the next trial begins, but
+    even if you don't please still respond."""))
 
   anykey = moment(display,"Hit any key to start the real experiment...")
   addbreak(anykey,await_response(iskeydown))
 
   for trial in 1:n_trials
-    addbreak_every(n_break_after,n_trials+n_break_after)
+    addbreak_every(n_break_after,n_trials+n_break_after/2)
     addtrial(real_trial(:medium,phase="test",trial == 1))
   end
 
@@ -179,18 +197,19 @@ setup(exp) do
   """)
   addbreak(message,await_response(iskeydown(key"`")))
 
-  addbreak(instruct("""
+  addbreak(cedrus_instruct("""
     You may have noticed that, on occasion, within a single trial,
     the sound switches between gallping and not galloping multiple times.
   """),
-  instruct("""
-    In the following trials hit "Q" if you hear one or no such switches. Hit "P"
-    if you hear more than one switch between galloping and not-galloping in a
-    single trial.
+  cedrus_instruct("""
+    In the following trials hit orange if you hear one or no switches. Hit 
+    the yellow if you hear more than one switch in a single trial.
+    It is possible you will never hit one of the buttons.
   """))
 
   for trial in 1:n_validate_trials
-    addtrial(validate_trial(:medium,phase="validate"))
+    addbreak_every(n_break_after,n_validate_trials)
+    addtrial(validate_trial(:medium,phase="validate",trial == 1))
   end
 end
 
--- stimtrak.jl
+++ stimtrak.jl
@@ -5,15 +5,20 @@ stop_record = 0xfd
 start_record = 0xfc
 manual_start_record = 0x29
 
-stimtrak(port) = daq_extension(
-  port,
-  eeg_sample_rate = 512,
-  codes = Dict(
-    "trial_start" => start_record,
-    "break_start" => stop_record,
-    "paused" => stop_record,
-    "unpaused" => start_record,
-    "terminated" => stop_record,
-    "UNUSED" => manual_start_record
-  )
+manual_codes = Dict(
+  "trial_start" => start_record,
+  "break_start" => stop_record,
+  "paused" => stop_record,
+  "unpaused" => start_record,
+  "terminated" => stop_record,
+  "UNUSED" => manual_start_record
 )
+
+code_names = ["stream_1","stream_2","stream_1_up","stream_2_up",
+               "switches_1_or_0","switches_2_or_more",
+               "stimulus_1","stimulus_2","stimulus_3"]
+
+codes = Dict(code => i for (i,code) in enumerate(code_names))
+merge!(codes,manual_codes)
+
+stimtrak(port) = daq_extension(port,eeg_sample_rate = 512,codes = codes)
