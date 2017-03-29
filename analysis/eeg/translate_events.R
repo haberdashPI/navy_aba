65cae1b05f627d1e4630cf3256dbb357034e2bf2
David Little <david.frank.little@gmail.com>
2017-03-29 07:47:00 -0700

    generalized translate events...

--- analysis/eeg/translate_events.R
+++ analysis/eeg/translate_events.R
@@ -2,75 +2,113 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-david = read.csv('data/David_events_2-22-17.csv') %>%
-    mutate(TriNo = as.numeric(as.character(TriNo)))
-david = david[order(davd$Time),]
-stimulus_events = david %>%
-    mutate(onset=lead(Time),latency = lead(Time) - Time) %>%
-    filter(TriNo %in% 7:9,lead(TriNo) == 256,latency < 0.2) %>%
-    select(TriNo,onset,latency)
+# change these fields to analyze another file
+file = '../../data/beatriz_2017_03_17/Beatriz_03_20_17.evt'
+out_file_prefix = '../../data/beatriz_2017_03_17/beatriz_stream_events'
 
-responses = david %>%
-    select(TriNo,Time) %>%
-    filter(TriNo %in% c(1:6,2048,4096))
+data = rbind(read.delim(file,sep='\t')) %>%
+    mutate(trigger = as.numeric(as.character(TriNo)),
+           time = Tmu/1000000) %>%
+    select(trigger,time)
+data = data[order(data$time),]
 
-resp1_down = responses %>%
-    mutate(latency = (lead(Time,2) - lead(Time))) %>%
-    mutate(onset = Time - latency) %>%
-    filter(TriNo == 1,lead(TriNo) == 2048,lead(TriNo,2) == 3) %>%
-    select(TriNo,onset,latency)
+stimulus_events = data %>%
+    mutate(onset=lead(time),latency = lead(time) - time) %>%
+    filter(trigger == 9,lead(trigger) == 256) %>%
+    select(trigger,onset,latency)
+mean(stimulus_events$latency < 0.03)
+stimulus_events = filter(stimulus_events,latency < 0.03)
+
+responses = data %>%
+    select(trigger,time) %>%
+    filter(trigger %in% c(1:4,4096,8192))
+
+## there was an error in the file: at some point cedrus responses were not
+## recorded by weber just use the responses recorded directly from cecdrus to
+## biosemi. unfortunately these are all offset rather than onset markers
 
 resp1_up = responses %>%
-    mutate(onset = Time,latency = lead(Time) - Time) %>%
-    filter(TriNo == 2048,lead(TriNo) == 3) %>%
-    mutate(TriNo = 3) %>%
-    select(TriNo,onset,latency)
-
-resp2_down = responses %>%
-    mutate(latency = (lead(Time,2) - lead(Time))) %>%
-    mutate(onset = Time - latency) %>%
-    filter(TriNo == 2,lead(TriNo) == 4096,lead(TriNo,2) == 4) %>%
-    select(TriNo,onset,latency)
+    filter(trigger == 4096) %>%
+    mutate(trigger = 3,latency=NA,onset=time) %>%
+    select(trigger,latency,onset)
 
 resp2_up = responses %>%
-    mutate(onset = Time,latency = lead(Time) - Time) %>%
-    filter(TriNo == 4096,lead(TriNo) == 4) %>%
-    mutate(TriNo = 4) %>%
-    select(TriNo,onset,latency)
+    filter(trigger == 8192) %>%
+    mutate(trigger = 4,latency=NA,onset=time) %>%
+    select(trigger,latency,onset)
 
-data = rbind(stimulus_events,resp1_down,resp1_up,resp2_down,resp2_up)
+data = rbind(stimulus_events,resp1_up,resp2_up)
+
+## remove anything with an onset eralier than 250
+## (this is the failed first block)
+## data = data %>% filter(onset > 250)
+old_data = data
+
+## insert stimulus triggers in the expected location when they are missing
+## (there are clear responses during these periods, indicating that the
+## stimuli were indeed presented)
+
+data = old_data
 data = data[order(data$onset),]
 
+trial_length = 2.8
+data = data %>%
+    filter(trigger %in% 9) %>%
+    filter(abs(onset - lag(onset) - 3*trial_length) < 0.5) %>%
+    mutate(onset = onset - trial_length) %>%
+    rbind(data)
+
+data = data[order(data$onset),]
+
+data = data %>%
+    filter(trigger %in% 9) %>%
+    filter(abs(onset - lag(onset) - 2*trial_length) < 0.5) %>%
+    mutate(onset = onset - trial_length,latency=NA) %>%
+    rbind(data)
+
+data = data[order(data$onset),]
+
+# calcualte trial by trial responses
 data_stim = data %>%
-    mutate(trial = cumsum(ifelse(TriNo == 7,1,0)) + 1) %>%
+    mutate(trial = cumsum(ifelse(trigger == 9,1,0)) + 1) %>%
     group_by(trial) %>%
-    mutate(resp = last(TriNo[TriNo %in% c(1,2)])) %>%
-    filter(TriNo %in% 7:9) %>%
+    mutate(resp = last(trigger[trigger %in% c(3,4)])) %>%
+    filter(trigger %in% 9) %>%
     ungroup() %>%
     mutate(real_resp = ifelse(is.na(resp),0,resp)) %>%
     fill(resp) %>%
     mutate(switching = ifelse(trial != lag(trial),resp != lag(resp),NA)) %>%
-    fill(switching)
-    
-data_stim[data_stim$trial == 2,'switching'] = FALSE
+    fill(switching) %>%
+    filter(!is.na(resp)) %>%
+    mutate(trial = trial - first(trial) + 1,trial_length = lead(onset) - onset)
 
-# TODO: how do we handle missed trial responses?
+data_stim[data_stim$trial == 1,'switching'] = FALSE
+SOA = 0.700
+data_stim = rbind(
+    data_stim,
+    data_stim %>% mutate(onset=onset+SOA,trigger=trigger+1),
+    data_stim %>% mutate(onset=onset+2*SOA,trigger=trigger+2)
+)
+data_stim = data_stim[order(data_stim$onset),]
+
+ggplot(data_stim,aes(y=as.numeric(lead(onset) - onset),x=onset)) +
+    geom_vline(data=subset(data,trigger <= 4),
+               aes(xintercept=onset,color=factor(trigger))) +
+    ## geom_vline(data=subset(data,trigger==9),
+    ##            aes(xintercept=onset),linetype=2) +
+    geom_point() + ylim(0,10) + xlim(1000,1200)
+
+ggplot(data_stim,aes(y=as.numeric(lead(onset) - onset),x=onset)) +
+    geom_point() + ylim(0,10)
 
 events = data_stim %>%
     mutate(Tmu = onset*10**6) %>%
-    mutate(TriNo = TriNo*100 + real_resp*10 + switching) %>%
+    mutate(real_resp = ifelse(real_resp == 0,2,real_resp)) %>%
+    mutate(trigger = (trigger-8)*100 + (real_resp-2)*10 + switching) %>%
     mutate(Code = 1) %>%
-    mutate(Comnt = '') %>%
-    select(Tmu,Code,TriNo,Comnt) %>%
-    mutate(TriNo = as.character(TriNo))
+    mutate(TriNo = as.character(trigger)) %>%
+    select(Tmu,Code,TriNo)
 
-write.table(rbind(data.frame(Tmu=0,Code=41,TriNo="2017-02-23T10:04:35.000",Comnt=""),events),
-            paste('david_stim_events_',Sys.Date(),'.evt',sep=''),
+write.table(rbind(data.frame(Tmu=0,Code=41,TriNo=as.character(Sys.Date())),events),
+            paste(out_file_prefix,Sys.Date(),'.evt',sep=''),
             row.names=F,sep='\t',quote=F)
-
-
-## TODO: code for individual beeps in aba, based on assumptions
-## of stimulus presentation.
-## different codes for different responeses
-## differnet codes for different trials
-# next version: one sound for all aba's
