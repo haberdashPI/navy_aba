83769053bb95127c180fb005263c2c2ead429a0b
David Little <david.frank.little@gmail.com>
2017-06-08 12:39:03 -0700

    udpated study2 pilot analysis

--- analysis/behavior/study2.R
+++ analysis/behavior/study2.R
@@ -4,67 +4,99 @@ require(tidyr)
 
 ## DESCRIPTION
 ##
-## Initial work on a plot for study1. It's not very good yet (c.f. TODO's)
+## Initial work on a plot for study1.
 
-## TODO: remove the first part of each trial
-## TODO: sort based on what response occured before deviant
+## data = rbind(read.csv('../../data/csv/david_study2_tone_0.0.1_6st_2017-06-01__08_37_38.csv'),
+##              read.csv('../../data/csv/david_study2_gap_0.0.1_6st_2017-06-01__07_13_43.csv'))
+## deviant_codes = c('tone','gap')
 
-data = rbind(read.csv('../../data/csv/david_study2_tone_0.0.1_6st_2017-06-01__08_37_38.csv'),
-             read.csv('../../data/csv/david_study2_gap_0.0.1_6st_2017-06-01__07_13_43.csv'))
+data = rbind(
+  read.csv('../../data/csv/abin_itt_study2_ripple_0.1.0_2017-06-08__08_47_55.csv'),
+  read.csv('../../data/csv/abin_study2_flash_0.1.0_2017-06-08__08_13_02.csv'),
+  read.csv('../../data/csv/test_study2_ripple_0.1.0_2017-06-08__10_39_46.csv'),
+  read.csv('../../data/csv/test_study2_flash_0.1.0_2017-06-08__10_04_28.csv'),
+  read.csv('../../data/csv/mary_study2_ripple_0.1.0_2017-06-08__09_36_26.csv'))
 
-## response proportion following disruption, and elsewhere
-lengths = c('gap' = 0.480, 'tone' = 1)
+deviant_codes = c('flash','ripple')
 
-deviants = data %>%
-  filter(code %in% c('gap','tone')) %>%
-  mutate(length = lengths[code])
-
-window_size = 4 # second
-bin_size = 0.25 # seconds
-
-minpos = function(xs) min(ifelse(xs > 0,xs,Inf))
-closest_positive = function(xs,ys){
-  X = outer(xs,ys,FUN="-")
-  apply(X,1,minpos)
-}
-
-code_nums = c('stream_1' = 0, 'stream_2' = 1)
-deviant_dists = data %>%
+# find last response for each row
+marked = data %>%
   select(trial,time,sid,deviant,code) %>%
-  group_by(deviant) %>%
-  mutate(
-    deviant_distance = closest_positive(time,time[code %in% c('gap','tone')]),
-    near_deviant = deviant_distance < window_size)
+  group_by(sid,deviant) %>%
+  mutate(duration = lead(time) - time) %>%
+  group_by(sid,deviant,trial,
+           response_index = cumsum(code %in% c('stream_1','stream_2'))) %>%
+  mutate(last_response = ifelse(first(code) %in% c('stream_1','stream_2'),
+                                as.character(first(code)),'none'))
 
-deviant_dists = deviant_dists %>%
-  group_by(deviant) %>%
-  mutate(duration = lead(time) - time)
+# find distance to deviant, and the response during the deviant
+marked_indexed = marked %>%
+  group_by(sid,deviant) %>%
+  mutate(deviant_index = cumsum(code %in% deviant_codes)) %>%
+  group_by(sid,deviant,deviant_index) %>%
+  mutate(deviant_distance = time - first(time),
+         response_on_deviant = first(last_response))
+
+## use only response after the first deviant
+## (maybe we should wait for bi-stability?)
+after_deviants = marked_indexed %>%
+  group_by(sid,deviant,trial) %>%
+  arrange(time) %>%
+  filter(deviant_index > 0) %>%
+  data.frame()
+
+## collect a window following each deviant
+
+## period in which a deviant cannot occur
+## 120ms = tone SOA
+## 120ms x 4 = 480ms = ABA SOA
+## 120ms x 4 x 2 = 1/2 of deviant spacing
+window_size = (120*4*6)/1000
+bin_size = 0.1 # seconds
 
 wmean = function(x,w) sum(x*w) / sum(w)
 wsd = function(x,w) (sum(w*(x-wmean(x,w))^2) / sum(w))
 
-props = deviant_dists %>%
-  group_by(deviant,near_deviant) %>%
-  arrange(time) %>%
-  mutate(window = floor((time - first(time))/window_size)) %>%
-  group_by(deviant,near_deviant,window) %>%
-  arrange(time) %>%
+deviant_time_course = after_deviants %>%
+  mutate(near_deviant = deviant_distance < window_size) %>%
+  group_by(sid,deviant,trial,deviant_index,response_on_deviant) %>%
   mutate(bin = floor((time - first(time))/bin_size)) %>%
-  group_by(deviant,near_deviant,bin) %>%
+  group_by(sid,deviant,trial,deviant_index,response_on_deviant,bin) %>%
   filter(code %in% c('stream_1','stream_2')) %>%
-  summarize(response = wmean(code_nums[as.character(code)],duration),
-            sd = wsd(code_nums[as.character(code)],duration),
-            N = length(unique(window)))
+  summarize(response = wmean(code == 'stream_2',duration))
 
-ggplot(props,aes(x=bin*bin_size,y=response,color=near_deviant)) +
-  geom_ribbon(aes(ymin=response - sd / sqrt(N),
-                  ymax=response + sd / sqrt(N),
-                  group = near_deviant),
-              color='white',fill='gray',alpha=0.6) +
-  geom_line() +
-  facet_wrap(~deviant) + xlab('Time (s)') + ylab('% streaming') +
-  ylim(0,1) +
-  theme_classic(base_size=16) + scale_color_brewer(palette='Set1')
+non_deviant_time_course = after_deviants %>%
+  filter(deviant_distance > 2*window_size) %>%
+  group_by(sid,deviant,trial,deviant_index) %>%
+  mutate(window = floor((time-first(time))/window_size)) %>%
+  group_by(sid,deviant,trial,deviant_index,window) %>%
+  mutate(first_response = first(last_response)) %>%
+  group_by(sid,deviant,trial,deviant_index,window,first_response) %>%
+  mutate(bin = floor((time - first(time))/bin_size)) %>%
+  group_by(sid,deviant,trial,deviant_index,first_response,window,bin) %>%
+  filter(code %in% c('stream_1','stream_2')) %>%
+  summarize(response = wmean(code == 'stream_2',duration))
 
-ggsave(paste('../../plots/study2_mean_response_',Sys.Date(),'.pdf',sep=''),
-       width=6,height=4)
+time_course = rbind(
+  deviant_time_course %>% data.frame() %>%
+    mutate(near_deviant=T,start_response = response_on_deviant) %>%
+    select(sid,deviant,trial,bin,response,near_deviant,start_response) %>%
+    data.frame(),
+  non_deviant_time_course %>% data.frame() %>%
+    mutate(near_deviant=F,start_response = first_response) %>%
+    select(sid,deviant,trial,bin,response,near_deviant,start_response) %>%
+    data.frame())
+
+for(csid in unique(time_course$sid)){
+  ggplot(time_course %>% filter(sid == csid,start_response != 'none'),
+         aes(x=bin*bin_size,y=response,color=near_deviant)) +
+    stat_summary(fun.data='mean_cl_boot',geom='smooth',
+                 fun.args=list(conf.int=0.75)) +
+    facet_grid(start_response~deviant) + xlab('Time (s)') + ylab('% streaming') +
+    ylim(0,1) + xlim(0,window_size) +
+    theme_classic(base_size=16) + scale_color_brewer(palette='Set1')
+
+  ggsave(paste('../../plots/study2_',csid,'_response_',Sys.Date(),'.pdf',sep=''),
+         width=6,height=4)
+
+}
